1ff084072773fa58c76ce712b197836c0220d55f
renanMendes <renan_oliveira_99@hotmail.com>
2023-09-11 15:28:22 -0300

    chancetointelij

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao meu catalágo!");
+        System.out.println("Filme: Dungeons & Dragons: Honra Entre Rebeldes");
+
+        int anoDeLancamento = 2023;
+        System.out.println("Ano de Lançamento:" + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 7.2;
+
+        }
+    }
