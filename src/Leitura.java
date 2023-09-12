29f5d17f7693cfe850a4ff7972ede82f7effd72c
renanMendes <renan_oliveira_99@hotmail.com>
2023-09-12 15:19:06 -0300

    Scanner

---
+++ src/Leitura.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito");
+        String filme = leitura.nextLine();
+
+
+        System.out.println("Qual o ano de lançamento? ");
+        int anoDeLancamento = leitura.nextInt();
+
+        System.out.println("avalie seu filme, de 0 a 10. ");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
+    }
+}
