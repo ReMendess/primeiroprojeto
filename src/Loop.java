1eb9186c3889b6b4472991824f8e5f8831d593ac
renanMendes <renan_oliveira_99@hotmail.com>
2023-09-13 15:09:59 -0300

    Loops

---
+++ src/Loop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Loop {
+
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("digite sua nota para o filme, de 0 a 10");
+            nota = leitura.nextDouble();
+            mediaAvaliacao += nota;
+        }
+
+        System.out.println("média de avaliações " + mediaAvaliacao / 3);
+
+
+    }
+}
